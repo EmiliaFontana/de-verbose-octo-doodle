e4a0ba3aae052543bd558d458960df6bbfb8aa8e
EmiliaFontana <gracielafernandez222@outlook.es>
2023-06-08 20:15:12 -0300

    contenedores

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,57 @@
-import { Application, Loader, Sprite } from 'pixi.js'
+import { Application, Container, Loader, Point, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	backgroundColor: 0x761BB2,
+	width: 1280,
+	height: 720
 });
 
+window.addEventListener("resize",()=>{
+	console.log("resized!");
+	const scaleX = window.innerWidth/app.screen.width;
+	const scaleY = window.innerHeight/app.screen.height;
+	const scale = Math.min(scaleX,scaleY);
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);	
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth)/2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight)/2);
+
+	app.view.style.width= gameWidth + "px";
+	app.view.style.height= gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+
+})
+window.dispatchEvent(new Event("resize"))
+
 Loader.shared.add({url:"./vege.png",name:"vegetta"})
-
+Loader.shared.add({url:"./gorra.png",name:"gorra"})
 Loader.shared.onComplete.add(()=>{
+	const gorra: Sprite = Sprite.from("gorra");
+	const vege: Sprite = Sprite.from("vegetta");
 
-	const clampy: Sprite = Sprite.from("vegetta");
-	
-	console.log(clampy.height, clampy.width)
+	console.log(vege.height, vege.width);
 
-	clampy.anchor.set(0.5);
+	const vegeygorro : Container = new Container();
+	vegeygorro.scale.set(0.5);
+
+	vegeygorro.x=200;
+	vegeygorro.y=300;
+
+	gorra.toGlobal(new Point());
 	
-	clampy.x = 400;
-	clampy.y = 400;
-	
-	app.stage.addChild(clampy);
+	vegeygorro.addChild(vege);
+	vegeygorro.addChild(gorra);
+	app.stage.addChild(vegeygorro);
 })
 
 Loader.shared.load();
